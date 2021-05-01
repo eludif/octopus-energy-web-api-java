5ae6200acd868d6fdcc4ce45c406c4c03b3be517
Fiyinfolu Eludire <17176053+faeludire@users.noreply.github.com>
2021-05-01 15:54:29 +0100

    tests: Rename GetProductsApiTest to DateFormatTest (#8)
    * tests: Rename GetProductsApiTest to DateFormatTest
    
    * Add workflow for develop branch
    
    * Rename maven.yml to workflow-develop.yml

--- src/test/java/com/wrapper/octopusenergy/DateFormatTest.java
+++ src/test/java/com/wrapper/octopusenergy/DateFormatTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class GetProductsApiTest {
+public class DateFormatTest {
 
     @Test
     public void shouldCorrectlyFormatDateToISO8601Format() {
